ca05e28e7466fed35014f66b93f24fe897dde83e
GitHub Action <action@github.com>
2021-04-19 08:11:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/adrijaned/intellijvhdlplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.adrijaned.intellijvhdlplugin.services
+
+import com.github.adrijaned.intellijvhdlplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
